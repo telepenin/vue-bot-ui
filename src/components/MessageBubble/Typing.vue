d1d8f663fe1f40805f72c257991d39be4de5da3b
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2023-06-09 12:41:28 +0100

    Bot typing procdedure

--- src/components/MessageBubble/Typing.vue
+++ src/components/MessageBubble/Typing.vue
@@ -4,6 +4,10 @@
     .qkb-msg-avatar__img &nbsp;
   .qkb-msg-bubble-component.qkb-msg-bubble-component--typing
     .qkb-msg-bubble-component__text
-      .qkb-msg-bubble__typing-indicator
-        span
+      .qkb-msg-bubble-component__container-text
+        .qkb-msg-bubble-component__thinking-text
+          | Thinking
+        .qkb-msg-bubble__typing-indicator-container
+          .qkb-msg-bubble__typing-indicator
+            span
 </template>
